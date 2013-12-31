95b6b70ae3c916ba4361f86adc23647722d9a9f1
michielpost <michiel@q42.nl>
2013-12-31 09:56:21 +0100

    Internet check in ScheduledAgent.cs

--- src/RijksLockScreen.WP.BackgroundTask/ScheduledAgent.cs
+++ src/RijksLockScreen.WP.BackgroundTask/ScheduledAgent.cs
@@ -5,6 +5,8 @@ using Windows.Phone.System.UserProfile;
 using RijksLockScreen.WP.Services;
 using Q42.WinRT;
 using System;
+using Microsoft.Phone.Net.NetworkInformation;
+using Microsoft.Phone.Info;
 
 namespace RijksLockScreen.WP.BackgroundTask
 {
@@ -43,59 +45,86 @@ namespace RijksLockScreen.WP.BackgroundTask
     /// </remarks>
     protected override async void OnInvoke(ScheduledTask task)
     {
-      if (LockScreenManager.IsProvidedByCurrentApplication)
+#if(DEBUG) 
+      var a = DeviceStatus.ApplicationCurrentMemoryUsage;
+      if (a != null) { }
+#endif
+
+      //Only run when there is internet connection
+      if (DeviceNetworkInformation.IsNetworkAvailable
+                   && (DeviceNetworkInformation.IsWiFiEnabled || DeviceNetworkInformation.IsCellularDataEnabled))
       {
-        string fileName = "lastUpdate";
-        Q42.WinRT.Storage.StorageHelper<DateTime?> sh = new Q42.WinRT.Storage.StorageHelper<DateTime?>(Q42.WinRT.Storage.StorageType.Local);
-        var lastUpdate = await sh.LoadAsync(fileName);
 
-        if (!lastUpdate.HasValue
-          || lastUpdate.Value.Date != DateTime.Now.Date)
+        if (LockScreenManager.IsProvidedByCurrentApplication)
         {
+          string fileName = "lastUpdate";
+          Q42.WinRT.Storage.StorageHelper<DateTime?> sh = new Q42.WinRT.Storage.StorageHelper<DateTime?>(Q42.WinRT.Storage.StorageType.Local);
+          var lastUpdate = await sh.LoadAsync(fileName);
 
-          // Get the URI of the lock screen background image.
-          // NOTE: GetImageUri throws is the app is not the current application 
-          var currentImage = LockScreen.GetImageUri();
+#if(DEBUG)
+          a = DeviceStatus.ApplicationCurrentMemoryUsage;
+          if (a != null) { }
+#endif
 
-          var rijksService = new RijksService();
-          var url = await rijksService.GetWeblUriAsync();
-
-          //Check if it's not already the current image
-          if (!currentImage.AbsolutePath.Contains(url.ToCacheKey()))
+          if (!lastUpdate.HasValue
+            || lastUpdate.Value.Date != DateTime.Now.Date)
           {
-            var localUri = rijksService.GetLocalImageUri(url);
-            await LockHelper.SetLock(url.AbsolutePath, false);
 
-            await sh.SaveAsync(DateTime.Now, fileName);
+            // Get the URI of the lock screen background image.
+            // NOTE: GetImageUri throws is the app is not the current application 
+            var currentImage = LockScreen.GetImageUri();
+
+            var rijksService = new RijksService();
+            var url = await rijksService.GetWeblUriAsync();
+
+ #if(DEBUG)  
+            a = DeviceStatus.ApplicationCurrentMemoryUsage;
+            if (a != null) { }
+#endif
+
+            //Check if it's not already the current image
+            if (!currentImage.AbsolutePath.Contains(url.ToCacheKey()))
+            {
+              var localUri = rijksService.GetLocalImageUri(url);
+              await LockHelper.SetLock(url.AbsolutePath, false);
+
+              await sh.SaveAsync(DateTime.Now, fileName);
+
+#if(DEBUG)  
+              a = DeviceStatus.ApplicationCurrentMemoryUsage;
+              if (a != null) { }
+#endif
+            }
+
           }
 
         }
+        //else
+        //{
+        //  // Do cleanup, since we are no longer the active lock screen provider.  
+        //  // This could be: delete images, stop the agent, etc...    
+        //  var periodicTask = ScheduledActionService.Find(task.Name) as PeriodicTask;
+        //  if (periodicTask != null)
+        //  {
+        //    try
+        //    {
+        //      ScheduledActionService.Remove(task.Name);
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //  }
+        //}
+
+
+        // If debugging is enabled, launch the agent again in one minute.
+        // debug, so run in every 30 secs
+#if(DEBUG)  
+      //ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(15)); 
+      //System.Diagnostics.Debug.WriteLine("Periodic task is started again: " + task.Name);
+#endif
 
       }
-      //else
-      //{
-      //  // Do cleanup, since we are no longer the active lock screen provider.  
-      //  // This could be: delete images, stop the agent, etc...    
-      //  var periodicTask = ScheduledActionService.Find(task.Name) as PeriodicTask;
-      //  if (periodicTask != null)
-      //  {
-      //    try
-      //    {
-      //      ScheduledActionService.Remove(task.Name);
-      //    }
-      //    catch (Exception)
-      //    {
-      //    }
-      //  }
-      //}
-
-
-      // If debugging is enabled, launch the agent again in one minute.
-      // debug, so run in every 30 secs
-#if(DEBUG)  
-      ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(15)); 
-      System.Diagnostics.Debug.WriteLine("Periodic task is started again: " + task.Name);
-#endif
 
       // Call NotifyComplete to let the system know the agent is done working.
       NotifyComplete();
